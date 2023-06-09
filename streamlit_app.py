99c3998a565639982c2f7096669f93e520031f96
bullah12 <116953705+bullah12@users.noreply.github.com>
2023-06-10 00:00:11 +0100

    Update streamlit_app.py
    deleted an unnecessary json response text

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,7 +20,6 @@ streamlit.dataframe(fruits_to_show)
 
 # New section to display FruityVice API
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
 
 streamlit.header("Fruityvice Fruit Advice!")
 
