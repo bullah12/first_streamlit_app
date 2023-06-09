68912200c304fcb0e14e398c2c908ac8c66a2295
bullah12 <116953705+bullah12@users.noreply.github.com>
2023-06-10 00:02:03 +0100

    Update streamlit_app.py
    modified so that the fruit can be manually chosen

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,9 +19,12 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 # New section to display FruityVice API
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 
 streamlit.header("Fruityvice Fruit Advice!")
+
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+fruityvice_response = requests.get(f"https://fruityvice.com/api/fruit/{fruit_choice}")
 
 # normalises the json code
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
