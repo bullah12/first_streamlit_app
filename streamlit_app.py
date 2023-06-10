5cb23882bfa7b5e7bea23acdb67265d0c3fe9659
bullah12 <116953705+bullah12@users.noreply.github.com>
2023-06-10 11:13:11 +0100

    Update streamlit_app.py
    converted adding new fruit to string literal

--- streamlit_app.py
+++ streamlit_app.py
@@ -68,7 +68,7 @@ if streamlit.button('Get Fruit Load List'):
 # Allow user to add fruits to the fruit table
 def insert_row_snowflake(new_fruit):
     with my_cnx.cursor() as my_cur:
-        my_cur.execute("insert into pc_rivery_db.public.fruit_load_list values ('jackfruit')")
+        my_cur.execute(f"insert into pc_rivery_db.public.fruit_load_list values ('{new_fruit}')")
         return "Thanks for adding " + new_fruit
         
 
